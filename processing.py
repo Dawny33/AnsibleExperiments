4f8c0de6c01d2e1081cfcc446e8a77dd823d8d87
Dawny33 <jrajrohit33@gmail.com>
2017-02-13 16:16:02 +0530

    Updated the code for using KMS

--- processing.py
+++ processing.py
@@ -8,8 +8,10 @@ This script takes the following iput:
 4. Full Bucket url
 """
 import os
-import boto3
 import sys
+import boto3
+from boto3 import session
+from botocore.client import Config
 
 
 N = str(sys.argv[1])
@@ -17,10 +19,6 @@ bucket_name = str(sys.argv[2])
 
 def recent_N(N, bucket_name):
 
-
-
-	#"s3cmd get $(s3cmd ls s3://episourceexperiment2/ | tail -5 | awk '{ print $4 }')"
-	#nrecent = 's3cmd ls ' + 's3://' + bucket_name + '/ | head -' + N +  "| awk '{ print $4 }'" + "| sed 's/s3:\/\/" + bucket_name +"\///'"
 	nrecent = 'aws s3 ls s3://' + bucket_name + '/ | sort | tail -n' + N + "| awk '{ print $4 }'"
 
 	output = os.popen(nrecent).read().split()
@@ -29,45 +27,22 @@ def recent_N(N, bucket_name):
 
 
 
-# print recent_N(str(5), "episourceexperiment2")
+def get_details(N, bucket_name):
 
-def get_details(N, bucket_path):
-
+	session = boto3.session.Session()
+	s3client = session.client('s3', config = boto3.session.Config(signature_version='s3v4'))
 	s3 = boto3.resource('s3')
-	files = recent_N(str(N), bucket_path)
-	bucket = s3.Bucket(bucket_path)
+	files = recent_N(str(N), bucket_name)
+	bucket = s3.Bucket(bucket_name)
 
 	for obj in bucket.objects.all():
 		key = obj.key
 		if key in files:
-			body = obj.get()['Body'].read()			
-			s3.Bucket('ansibletest1').put_object(Key=key, Body=body)
-			           
+			body = s3client.get_object(Bucket=bucket_name, Key=key)['Body'].read()
+			s3client.put_object(Key=key, Body=body, Bucket='ansibletest1')
 
 
 
 
 
 print get_details(N, str(bucket_name))
-
-
-
-
-
-
-
-
-
-
-'''
-
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('test-bucket')
-# Iterates through all the objects, doing the pagination for you. Each obj
-# is an ObjectSummary, so it doesn't contain the body. You'll need to call
-# get to get the whole body.
-for obj in bucket.objects.all():
-    key = obj.key
-    body = obj.get()['Body'].read()
-
-'''
