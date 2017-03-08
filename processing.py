9ff96b1fe4ba81ab740c18c2d7ae7e2c1e059f49
Dawny33 <jrajrohit33@gmail.com>
2017-03-08 16:55:20 +0530

    Updated Code

--- processing.py
+++ processing.py
@@ -1,48 +1,43 @@
-#!/usr/bin/python
-
-"""
-This script takes the following iput:
-1. No. of files to be processed
-2. Last processed time
-3. No. of slaves to be launched
-4. Full Bucket url
-"""
 import os
 import sys
 import boto3
 from boto3 import session
 from botocore.client import Config
+import json
+import base64
+
+# Get the service resource
+sqs = boto3.resource('sqs')
+
+# Get the queue
+#queue = sqs.get_queue_by_name(QueueName='DawnyQueue')
+queue = sqs.Queue('https://us-west-2.queue.amazonaws.com/080983167913/DawnyQueue')
+dynamo = boto3.resource('dynamodb')
+table = dynamo.Table('iocheck')
+
+messages_available = queue.attributes['ApproximateNumberOfMessages']
+
+# Process messages by printing out body and optional author name
+for message in queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=5):
+
+    session = boto3.session.Session()
+    s3client = session.client('s3', config = boto3.session.Config(signature_version = 's3v4'))
+    s3 = boto3.resource('s3')
+
+    bucket_name = 'sourcebucketepi'
+
+    body_json = json.loads(message.body)
+
+    sqs_key = body_json['Records'][0]['s3']['object']['key']
+
+    decoded_key = str(base64.decodestring("OTAzXzEwLnR4dA%3D%3D%0A".replace('%3D', '=').replace('%0A', '')))
+
+    table.update_item(Key={'filename': sqs_key ,'status': 'Done',})
 
 
-N = str(sys.argv[1])
-bucket_name = str(sys.argv[2])
+    body = s3client.get_object(Bucket=bucket_name, Key=decoded_key)['Body'].read()
+    s3client.put_object(Key=sqs_key, Body=body, Bucket='destbucketepi')
 
-def recent_N(N, bucket_name):
+    message.delete()
 
-	nrecent = 'aws s3 ls s3://' + bucket_name + '/ | sort | tail -n' + N + "| awk '{ print $4 }'"
-
-	output = os.popen(nrecent).read().split()
-
-	return output
-
-
-
-def get_details(N, bucket_name):
-
-	session = boto3.session.Session()
-	s3client = session.client('s3', config = boto3.session.Config(signature_version='s3v4'))
-	s3 = boto3.resource('s3')
-	files = recent_N(str(N), bucket_name)
-	bucket = s3.Bucket(bucket_name)
-
-	for obj in bucket.objects.all():
-		key = obj.key
-		if key in files:
-			body = s3client.get_object(Bucket=bucket_name, Key=key)['Body'].read()
-			s3client.put_object(Key=key, Body=body, Bucket='ansibletest1')
-
-
-
-
-
-print get_details(N, str(bucket_name))
+    # Let the queue know that the message is processed
